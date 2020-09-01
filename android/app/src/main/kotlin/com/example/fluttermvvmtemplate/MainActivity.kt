a7a2572983b8afbf03d61e920ed92ce784c2c3a8
vb <vbacik.10@gmail.com>
2020-09-01 22:32:49 +0300

    android ios fasltane process complete

--- android/app/src/main/kotlin/com/example/fluttermvvmtemplate/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fluttermvvmtemplate
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/hwa/fluttermvvmtemplate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hwa.fluttermvvmtemplate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
