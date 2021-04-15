9c0761c0581ee04fedef21396033107c7d3711b6
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-04-16 00:03:51 +0300

    Variable name fixed.

--- lib/core/base/state/base_state.dart
+++ lib/core/base/state/base_state.dart
@@ -5,6 +5,6 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> {
 
   double dynamicHeight(double value) =>
       MediaQuery.of(context).size.height * value;
-  double dyanmicWidth(double value) =>
+  double dynamicWidth(double value) =>
       MediaQuery.of(context).size.width * value;
 }
